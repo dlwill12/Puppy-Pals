7525e6fc477709944552fbdeb6d01e816304b239
dlwill12 <wrecwill@gmail.com>
2023-09-25 16:05:16 -0500

    "re-submit"

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import "./App.css";
 import { puppyList } from "./data.js";
 import "./index.css";
 
+// React functional componet
 function App() {
+  //invoke the 'useState' HOOK to INITIALIZE two 'state' variables: => puppies &&  => featPupId.
+  //useState returns an array
   const [puppies, setPuppies] = useState(puppyList);
+  // puppies represent the puppy
   const [featPupId, setFeatPupId] = useState(null);
 
   function handleClick() {
@@ -12,31 +16,39 @@ function App() {
   }
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
   console.log(featuredPup);
+
   return (
-    <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p
-            onClick={() => {
-              setFeatPupId(puppy.id);
-            }}
-            key={puppy.id}
-          >
-            {puppy.name}
-          </p>
-        );
-      })}
-      {featPupId && (
-        <div className="idInfo">
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
-    </div>
+    <>
+      <div className="App">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+
+        {/* if featPupId in not null render */}
+        {featPupId && (
+          <div className="idInfo">
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
 export default App;
+{
+  /* <button onClick={() => setFeatPupId(null)}>Back to List</button> */
+}
